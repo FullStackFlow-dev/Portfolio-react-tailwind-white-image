458fc072d5ca6b61e0158071c68624493bb28a7d
Merph. Dev-full (2) <merphy97@gmail.com>
2026-02-06 18:30:43 +0000

    Fix GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,6 @@ import react from '@vitejs/plugin-react'
 // Configuration de Vite pour React
 // Vite est un outil de build moderne et ultra-rapide
 export default defineConfig({
-  base: process.env.GITHUB_ACTIONS
-    ? '/Portfolio-react-tailwind-white-image/'
-    : '/',
+  base: './',
   plugins: [react()], // Active le support de React (JSX, Fast Refresh)
 })
